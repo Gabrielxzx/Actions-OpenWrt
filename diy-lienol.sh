6c7ed761404bdaefbf0540cd1f1787fc0c8d6942
Gabrielxzx <Gabrielx_zx@163.com>
2020-04-11 19:25:16 +0800

    Create diy-lienol.sh
    加入adguardhome与lienol大包

---
+++ diy-lienol.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#=================================================
+# Description: DIY script
+# Lisence: MIT
+# Author: P3TERX
+# Blog: https://p3terx.com
+#=================================================
+# Modify default IP
+#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+#添加adguardhome
+git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
+#添加lienol包
+git clone https://github.com/Lienol/openwrt-package  package/lienol/
+
+./scripts/feeds update -a
+./scripts/feeds install -a
