c0edbaa41fb566d7b332a57f0373a5c9e629bce6
Gabrielxzx <Gabrielx_zx@163.com>
2020-05-27 20:00:44 +0800

    Update diy-lienol.sh

--- diy-lienol.sh
+++ diy-lienol.sh
@@ -9,16 +9,19 @@
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
 #添加adguardhome
-git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
+#git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
 
 #添加ssrplus
 #git clone https://github.com/fw876/helloworld.git package/helloworld
-git clone https://github.com/Gabrielxzx/helloworld.git package/helloworld
+#git clone https://github.com/Gabrielxzx/helloworld.git package/helloworld
 
 #添加lienol包
-git clone https://github.com/Gabrielxzx/lienol-openwrt-package.git package/lienol
+#git clone https://github.com/Gabrielxzx/lienol-openwrt-package.git package/lienol
 #git clone https://github.com/Lienol/openwrt-package  package/lienol/
 
+#添加整合包
+git clone https://github.com/kenzok8/openwrt-packages.git package/kenzok8
+git clone https://github.com/kenzok8/small.git package/small
 
 #更新feeds
 ./scripts/feeds update -a
