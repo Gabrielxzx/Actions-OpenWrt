8b4a83fbf06f5f22ce546b48ecde97f3ffd62cd2
Gabrielxzx <Gabrielx_zx@163.com>
2021-01-05 21:08:37 +0800

    Update diy-part2.sh

--- scripts/diy-part2.sh
+++ scripts/diy-part2.sh
@@ -10,3 +10,4 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.2.1/192.168.2.100/g' package/base-files/files/bin/config_generate
