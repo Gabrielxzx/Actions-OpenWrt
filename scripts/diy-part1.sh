3620fc6101f51808aa1e320fb99a5831200a1557
Gabrielxzx <Gabrielx_zx@163.com>
2021-01-10 11:39:58 +0800

    Update diy-part1.sh

--- scripts/diy-part1.sh
+++ scripts/diy-part1.sh
@@ -21,11 +21,13 @@
 
 #添加整合包
 git clone https://github.com/kenzok8/openwrt-packages.git package/kenzok8
-#git clone https://github.com/Gabrielxzx/openwrt-packages.git package/Gabrielx
 git clone https://github.com/kenzok8/small.git package/small
 
+#添加京东插件
+git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/luci-app-jd-dailybonus
+
 #添加luci-app-dockerman
-git clone https://github.com/lisaac/luci-app-dockerman.git package/dockerman
+#git clone https://github.com/lisaac/luci-app-dockerman.git package/dockerman    #暂时编译失败
 
 #更新feeds
 ./scripts/feeds update -a
