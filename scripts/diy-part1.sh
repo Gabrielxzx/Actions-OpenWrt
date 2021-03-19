b1218c494739d0b0a56f50f425e168ee41228198
Gabrielx <gabrielx_zx@163.com>
2021-03-19 18:07:54 +0800

    update diy-part1.sh

--- scripts/diy-part1.sh
+++ scripts/diy-part1.sh
@@ -5,8 +5,6 @@
 # Author: P3TERX
 # Blog: https://p3terx.com
 #=================================================
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
 #添加adguardhome
 #git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
@@ -18,6 +16,9 @@
 #git clone https://github.com/Gabrielxzx/lienol-openwrt-package.git package/lienol
 #git clone https://github.com/xiaorouji/openwrt-package.git  package/lienol/
 
+#添加jd-dailybonus
+git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git  package/jd-dailybonus
+
 #添加整合包
 git clone https://github.com/kenzok8/small.git package/small
 git clone https://github.com/kenzok8/openwrt-packages.git package/kenzok8
@@ -27,11 +28,7 @@ git clone https://github.com/lisaac/luci-app-dockerman.git package/dockerman
 
 #添加luci-app-godproxy
 git clone https://github.com/project-lede/luci-app-godproxy.git package/luci-app-godproxy
-#git clone https://github.com/Gabrielxzx/luci-app-godproxy.git package/luci-app-godproxy
 
-#添加luci-app-vssr   #kenzok8整合包中已有
-#git clone https://github.com/jerrykuku/lua-maxminddb.git package/lua-maxminddb
-#git clone https://github.com/jerrykuku/luci-app-vssr.git package/vssr
 
 #更新feeds
 ./scripts/feeds update -a
