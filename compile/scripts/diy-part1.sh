46d4b28938955f42dad488fd5218b1343d682bea
Gabrielx <gabrielx_zx@163.com>
2021-04-24 10:13:51 +0800

    重排文件结构

--- compile/scripts/diy-part1.sh
+++ compile/scripts/diy-part1.sh
@@ -9,16 +9,9 @@
 #添加adguardhome
 #git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
 
-#添加ssrplus
-#git clone https://github.com/fw876/helloworld.git package/helloworld
-
 #添加整合包
-git clone https://github.com/kenzok8/small.git package/small
-git clone https://github.com/kenzok8/openwrt-packages.git package/kenzok8
-#git clone https://github.com/Gabrielxzx/openwrt-packages.git package/kenzok8
-
-#添加luci-app-dockerman
-git clone https://github.com/lisaac/luci-app-dockerman.git package/dockerman
+#git clone https://github.com/kenzok8/small.git package/small
+#git clone https://github.com/kenzok8/openwrt-packages.git package/kenzok
 
 #添加luci-app-godproxy
 git clone https://github.com/project-lede/luci-app-godproxy.git package/luci-app-godproxy
