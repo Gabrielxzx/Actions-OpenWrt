c460c8b314f2ec6e7e9425216e0581d25e49d641
Gabrielx <gabrielx_zx@163.com>
2022-03-27 20:44:27 +0800

    广告屏蔽大师增加默认规则：anti-AD

---
+++ files/usr/share/adbyby/adblock.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+uclient-fetch --no-check-certificate -O - 'https://easylist-downloads.adblockplus.org/easylistchina+easylist.txt' > /tmp/adnew.conf
+uclient-fetch --no-check-certificate -O - 'https://anti-ad.net/anti-ad-for-dnsmasq.conf' > /usr/share/adbyby/rules/data/anti-ad-for-dnsmasq.conf
+
+if [ -s "/tmp/adnew.conf" ];then
+  /usr/share/adbyby/ad-update
+fi
+
+rm -f /tmp/adbyby.updated 
+sleep 10
+/etc/init.d/adbyby restart
