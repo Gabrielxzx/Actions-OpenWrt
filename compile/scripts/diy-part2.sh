56c9dd2c07b481dc99573e0a52126d9bb4d37202
Gabrielxzx <gabrielx_zx@163.com>
2023-04-16 08:46:23 +0800

    fix adguardhome

--- compile/scripts/diy-part2.sh
+++ compile/scripts/diy-part2.sh
@@ -33,8 +33,9 @@ wget -P files/usr/share/adbyby/data https://raw.githubusercontent.com/Gabrielxzx
 
 # Fix luci-app-adguardhome
 wget -P files/etc/init.d https://raw.githubusercontent.com/Gabrielxzx/Gabrielxzx-luci/master/files/luci-app-adguardhome/adguardhome
+wget -P files/etc/init.d https://raw.githubusercontent.com/Gabrielxzx/Gabrielxzx-luci/master/files/luci-app-adguardhome/init.d/AdGuardHome
 wget -P files/usr/bin https://raw.githubusercontent.com/Gabrielxzx/Gabrielxzx-luci/master/files/luci-app-adguardhome/core/AdGuardHome
-wget -P files/etc https://raw.githubusercontent.com/Gabrielxzx/Gabrielxzx-luci/master/files/luci-app-adguardhome/AdGuardHome.yaml
+wget -P files/etc/config https://raw.githubusercontent.com/Gabrielxzx/Gabrielxzx-luci/master/files/luci-app-adguardhome/AdGuardHome.yaml
 
 # Fix luci-app-openclash
 wget -P files/etc/openclash/core https://raw.githubusercontent.com/Gabrielxzx/Gabrielxzx-luci/master/files/luci-app-openclash/core/clash
